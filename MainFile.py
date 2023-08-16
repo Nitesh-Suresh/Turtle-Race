94d623aacc0fe22d4361befe525f49692e378ac6
Nitesh-Suresh <niteshanitha@gmail.com>
2023-08-16 10:21:39 +0530

    Update MainFile.py
    Small changes in the code

--- MainFile.py
+++ MainFile.py
@@ -2,54 +2,52 @@
 Turtle Race
 """
 
-from asyncore import write
-from operator import truediv
-import turtle, random
+import turtle
+import random
+import time
+
+# Initialize the turtle
 r = turtle.Turtle()
 r.speed(1)
 
+# Define constants
 x=300
 y=270
+
+# Get the number of players from user
 n=int(input("Enter the number players: "))
 
+# Lists to store player names and turtle objects
 listofnames=[]
 list_players=[]
+start_times={}
 
 
-#1 Draw a play ground
+#Function to Draw a play ground
 def draw_playground():
     r.pensize(5)
-    r.pencolor("blue")
-    #r.begin_fill()
+    r.pencolor("gray")
     r.penup()
     r.goto(-x,-x)
     r.pendown()
-    r.forward(x*2)
-    r.left(90)
-    r.forward(y*2)
-    r.left(90)
-    r.forward(x*2)
-    r.left(90)
-    r.forward(y*2)
-    r.left(90)
-    #r.end_fill()
+    for _ in range(2):
+        r.forward(x * 2)
+        r.left(90)
+        r.forward(y * 2)
+        r.left(90)
 
-#2 Display the title
+#2 Function to Display the title
 def print_title():
     r.penup()
     r.goto(0,y)
-    r.pendown
-
-    r.pencolor("Green")
-
-
+    r.pendown()
+    r.pencolor("darkblue")
     r.write(
         "T U R T L E  R A C E",
         align='center',
-        font=('Aria',30,'bold'))
+        font=('Arial',30,'bold'))
 
-
-#3 Display Start and Draw a start line
+#3 Function to Display Start and Draw a start line
 def start_line():
     r.penup()
     r.goto(-230,180)
@@ -59,11 +57,11 @@ def start_line():
         'START', 
         align="center",
         font=('Times',18,'bold')
-        )
+    )
     r.goto(-230,-250)
 
 
-#4 Display finish line amd draw a finish line
+#4 Function to Display finish line amd draw a finish line
 def finish_line():
     r.penup()
     r.goto(230,180)
@@ -73,57 +71,79 @@ def finish_line():
         'FINISH', 
         align="center",
         font=('Arial',18,'bold')
-        )
+    )
     r.goto(230,-250)
 
-#5 Create a list of 8 turtle and place it before start line
-
+#5 Function to create players (turtles)
 def createplayers():
     gap=0
     for i in range(n):
-        print("Enter the name of person",i+1)
-        name=input()
+        name=input("Enter the name of person "+str(i+1)+": ")
         listofnames.append(name)
         t=turtle.Turtle()
         t.shape("turtle")
         t.penup()
         t.goto(-260,160-gap)
-        gap=gap+40
-        t.write(listofnames[i], align='right', font=("Courier New",8,"bold"))
+        gap+=40
+        t.write(
+            listofnames[i], 
+            align='right', 
+            font=("Courier New",8,"bold")
+        )
         t.forward(10)
         t.pendown()
         list_players.append(t)
 
 
 
-#6 make all the turtle move towards finish line using random module
-#7 Check if the turtle has reached finish line
-#8. Display the winner
+#Function to start the race
 def start_the_race():
     foundwinner=False
-    low=0
-    high=5
-    while(True):
+
+    for i in range(n):
+        start_times[i] = time.time()
+    while not foundwinner:
         for i in range(n):
-            list_players[i].forward(random.randint(low,high))
+            list_players[i].forward(random.randint(0,5))
 
             if list_players[i].xcor()>230:
                 winner_index=i
                 foundwinner=True
                 break
 
-        if foundwinner:
-            r.penup()
-            r.goto(0,0)
-            r.pendown()
-            s = listofnames[winner_index] + ' is the winner'
-            r.write(s, align='center',font=('Courier New',20,'bold'))
-            break
-        
+    r.penup()
+    r.goto(0, 0)
+    r.pendown()
+    winner_message = listofnames[winner_index] + ' is the winner'
+    r.write(
+        winner_message, 
+        align='center', 
+        font=('Courier New', 20, 'bold')
+    )
 
+    # Calculate and display time taken by each player
+    for i in range(n):
+        end_time = time.time()
+        time_taken = end_time - start_times[i]
+        t = list_players[i]
+        t.penup()
+        t.goto(-230, t.ycor())
+        t.write(
+            f'Time: {time_taken:.2f} seconds',
+            align='center',
+            font=('Arial', 12, 'normal')
+        )
+        
+# Main game logic
 draw_playground()
 print_title()
 start_line()
 finish_line()
 createplayers()
 start_the_race()
+
+# Keep the window open for 7 seconds
+time.sleep(7)
+
+# Close the turtle graphics window
+turtle.bye()
